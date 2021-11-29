8e7fe75984fa7ee19c62aba5e5751e65d17595e1
swapnil-bagul <bagulswapnilp@gmail.com>
2021-11-29 21:56:46 +0530

    [Swapnil] Added UC3 Retrive Selected Data

--- ProductReviewManagement/ProductManagement.cs
+++ ProductReviewManagement/ProductManagement.cs
@@ -27,5 +27,23 @@ namespace ProductReviewManagement
                 Console.WriteLine("ProductId:-" + list.ProductId + " UserId:-" + list.UserId + " Ratings:-" + list.Rating + " Review:-" + list.Review + " IsLike:-" + list.isLike);
             }
         }
+        /// <summary>
+        /// Selected records with where condition UC3
+        /// </summary>
+        /// <param name="listReview">The list review.</param>
+        public void SelectedRecords(List<ProductReview> listReview)
+        {
+            //lambda syntax
+            var recordData = listReview.Where(r => r.Rating > 3 && (r.ProductId == 1 || r.ProductId == 4 || r.ProductId == 9)).ToList();
+            //query syntax
+            var recordedData = (from productReviews in listReview
+                                where productReviews.Rating > 3 && (productReviews.ProductId == 1 || productReviews.ProductId == 4 || productReviews.ProductId == 9)
+                                select productReviews);
+            foreach (var list in recordedData)
+            {
+                Console.WriteLine("ProductId:-" + list.ProductId + " UserId:-" + list.UserId + " Ratings:-" + list.Rating + " Review:-" + list.Review + " IsLike:-" + list.isLike);
+            }
+        }
+
     }
 }
